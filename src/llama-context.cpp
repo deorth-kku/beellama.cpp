ggml-org/llama.cpp

server : encode images on the GPU by borrowing the idle compute buffers
When the projector does not fit alongside the model, the only option is
--no-mmproj-offload, which puts the vision encoder on the CPU. The memory for it
is in fact available, it is just spoken for: the per-ubatch compute buffers are
worst-case scratch and sit idle for the whole duration of an image encode.

Two additions let them be lent out:

* llama_sched_release() frees the scheduler and with it the graph allocator's
  buffers, and sets the re-reserve flag. sched_reserve() already runs at the top
  of encode() and decode(), so the next call rebuilds them from the same
  worst-case graph. No weights or KV cache are touched.

* mtmd_batch_set_ctx() re-points an encoded batch at another context of the same
  mmproj file. mtmd_batch_encode() leaves its result in host memory owned by the
  batch, so the context that produced it is only needed for the embedding width
  afterwards, and can be freed before the decode consumes the embeddings.

Behind --mmproj-vram-swap the server keeps the host projector loaded as before
and, per image batch, releases the compute buffers, loads a GPU-resident
projector into that space, encodes, re-points the batch and frees the projector
again. Nothing migrates between devices: the transient context reads the same
file. Falls back to a host encode if the GPU load fails, which costs nothing
extra because the released buffers are re-reserved by the next decode either
way. Ignored with a warning when the projector is already offloaded or absent.

Measured on 2x Tesla V100-SXM2-16GB, Qwen3.8-27B-abliterated-Q6_K with
mmproj-F16 (885 MiB), -sm tensor -ts 1,1 -fa on, n_ctx 150016, n_ubatch 1024,
1048-token image prompt:

    --no-mmproj-offload    20.0 - 22.4 s of prompt eval
    --mmproj-vram-swap      3.6 -  3.8 s

Of the latter, 1.5-1.8 s is mtmd_init_from_file() and ~2.05 s is the model
prefilling the 1048 embedding positions, which is the same work as 1048 text
tokens.

src/llama-context.cpp
@@ -579,6 +579,38 @@ void llama_context::resolve_fused_ops(const llama_memory_context_i * mctx, uint3
     }
 }
 
+void llama_context::sched_release() {
+    if (!sched) {
+        return;
+    }
+
+    // Hand the compute buffers back. The scheduler owns them through its
+    // graph allocator, so freeing it is the only way to release the
+    // per-ubatch scratch - on a large context that is gigabytes (here:
+    // ~2.4 GiB of FlashAttention F16 scratch plus ~0.6 GiB of activations at
+    // n_ctx = 150k, n_ubatch = 1024), and it sits idle between requests.
+    //
+    // Nothing is lost: sched_reserve() runs at the top of both encode() and
+    // decode(), so the next call rebuilds the scheduler from the same
+    // worst-case graph. The caller only pays the reserve, not a reload.
+    synchronize();
+
+    const size_t n_bufs = backend_ptrs.size();
+    size_t released = 0;
+    for (size_t i = 0; i < n_bufs; i++) {
+        released += ggml_backend_sched_get_buffer_size(sched.get(), backend_ptrs[i]);
+    }
+
+    gf_res_prev.reset();
+    gf_res_reserve.reset();
+    sched.reset();
+
+    sched_need_reserve = true;
+
+    LLAMA_LOG_INFO("%s: released %.2f MiB of compute buffers\n",
+            __func__, released / (1024.0 * 1024.0));
+}
+
 void llama_context::sched_reserve() {
     if (!sched_need_reserve) {
         return;
@@ -3844,6 +3876,10 @@ void llama_set_warmup(llama_context * ctx, bool warmup) {
 
 void llama_synchronize(llama_context * ctx) {
     ctx->synchronize();
+}
+
+void llama_sched_release(llama_context * ctx) {
+    ctx->sched_release();
 }
 
 float * llama_get_logits(llama_context * ctx) {

deorth-kku/beellama.cpp

src/llama-context.cpp
@@ -614,6 +614,40 @@ static int llama_graph_n_input_tensors(ggml_cgraph * gf) {
     return (int) users.size();
 }
 
+void llama_context::sched_release() {
+    if (!sched) {
+        return;
+    }
+
+    // Hand the compute buffers back. The scheduler owns them through its
+    // graph allocator, so freeing it is the only way to release the
+    // per-ubatch scratch - on a large context that is gigabytes (here:
+    // ~2.4 GiB of FlashAttention F16 scratch plus ~0.6 GiB of activations at
+    // n_ctx = 150k, n_ubatch = 1024), and it sits idle between requests.
+    //
+    // Nothing is lost: sched_reserve() runs at the top of both encode() and
+    // decode(), so the next call rebuilds the scheduler from the same
+    // worst-case graph. The caller only pays the reserve, not a reload.
+    synchronize();
+
+    const size_t n_bufs = backend_ptrs.size();
+    size_t released = 0;
+    for (size_t i = 0; i < n_bufs; i++) {
+        released += ggml_backend_sched_get_buffer_size(sched.get(), backend_ptrs[i]);
+    }
+
+    for (auto & res : gf_res_prev) {
+        res.reset();
+    }
+    gf_res_reserve.reset();
+    sched.reset();
+
+    sched_need_reserve = true;
+
+    LLAMA_LOG_INFO("%s: released %.2f MiB of compute buffers\n",
+            __func__, released / (1024.0 * 1024.0));
+}
+
 void llama_context::sched_reserve() {
     if (!sched_need_reserve) {
         return;
@@ -3927,6 +3961,10 @@ void llama_set_warmup(llama_context * ctx, bool warmup) {
 
 void llama_synchronize(llama_context * ctx) {
     ctx->synchronize();
+}
+
+void llama_sched_release(llama_context * ctx) {
+    ctx->sched_release();
 }
 
 float * llama_get_logits(llama_context * ctx) {